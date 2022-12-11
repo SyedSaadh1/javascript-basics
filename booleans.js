080a32a30e307f292af8df4e8dfd1336afde659d
SyedSaadh1 <119276844+SyedSaadh1@users.noreply.github.com>
2022-12-11 13:03:32 +0530

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,12 @@
+true or false
+0 = false
+1 = true
+
+5 == 6
+false
+5 == 5
+true
+5 == 100
+false
+10 == 10
+true
---
+++ strings.js
@@ -0,0 +1,25 @@
+"helloWorld"
+'helloWorld'
+`helloWorld`
+
+"I am from \"ongole\""
+
+'I am from \'ongole'
+
+"Name: syed saadh city: ongole Designation: UI Dev"
+
+`
+Name: syed saadh
+city: ongole
+Designation: UI Dev
+`
+`5 plus 5 is ${5+5}`
+
+> `5 plus 5 is 5+5`
+'5 plus 5 is 5+5'
+
+> `5 plus 5 is ${5+5}`
+'5 plus 5 is 10'
+
+> "5 plus 5 is ${5+5}"
+'5 plus 5 is ${5+5}'
